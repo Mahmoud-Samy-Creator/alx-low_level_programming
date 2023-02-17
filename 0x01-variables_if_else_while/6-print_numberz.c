2230e16d1a425f8f210e3bb343fbc0171a3131c2
Mahmoud Samy <mahmodsmsm43@gmail.com>
2023-02-17 13:12:55 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,3 +4,16 @@
  * while loop - printing alphabets lower and upper case
  * Return: 0
 */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
