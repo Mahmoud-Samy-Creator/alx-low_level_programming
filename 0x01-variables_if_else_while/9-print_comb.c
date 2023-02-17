56ddd3a19caab66672ca0d24b1abc51294019dbd
Mahmoud Samy <mahmodsmsm43@gmail.com>
2023-02-17 14:21:26 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,15 @@ int main(void)
 {
 	int num;
 
-        for (num = 0 ; num < 10 ; num++)
-        {
-                putchar(num + 48);
-                if (num != 9)
-                {
-                        putchar(',');
-                        putchar(' ');
-                }
-        }
-        putchar('\n');
-        return (0);
+	for (num = 0 ; num < 10 ; num++)
+	{
+		putchar(num + 48);
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
