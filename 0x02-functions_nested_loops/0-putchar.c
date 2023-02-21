dc2d4606e028dee74d11cddf59c91a5e01585a38
Mahmoud Samy <mahmodsmsm43@gmail.com>
2023-02-21 14:28:39 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,8 @@
-#include <stdio.h>
-#include <string.h>
+#include <main.h>
+/**
+ * main - Entry point
+ * Return:0
+ */
 int main(void)
 {
 	putchar('_');
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+ #include "main.h"
+>
+> /**
+>  * main - Entry point
+>  * Return:0
+>  */
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return:0
+ */
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return:0
+ */
