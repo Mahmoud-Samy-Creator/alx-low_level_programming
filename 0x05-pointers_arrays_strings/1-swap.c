dc613369a6e8b6fc8a6df04c0a698c4a5bc54c52
Mahmoud Samy <mahmodsmsm43@gmail.com>
2023-02-27 11:17:40 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - swap argument values
+ * @a: pointer (1)
+ * @b: pointer (2)
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int p;
+	p = *a;
+	*a = *b;
+	*b = p;
+}
