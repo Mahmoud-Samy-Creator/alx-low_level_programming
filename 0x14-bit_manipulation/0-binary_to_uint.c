4dfe693a351ebc1a28e4078810382abec7785197
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-04 22:56:05 +0300

    0-binary_to_uint.c: function converts a binary to an decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,6 +6,37 @@
  * Returns: int
  */
 
+/**
+ * _pow - a a function for power
+ * @x: base number
+ * @y: power number
+ * Returns: int
+ */
+
+int _pow(int base, int exponent)
+{
+	int i = 0;
+	int result = 1;
+
+	if (exponent == 0)
+		return (1);
+
+	else if (exponent > 0)
+	{
+		for (i = 0 ; i < exponent ; i++)
+			result *= base;
+	}
+
+	else
+	{
+		for (i = 0 ; i > exponent ; i--)
+			result /= base;
+	}
+
+	return (result);
+}
+
+
 unsigned int binary_to_uint(const char *b)
 {
 
