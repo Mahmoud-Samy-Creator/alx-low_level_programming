c3fe63501a67e08a1b8a289295ac1216324100b0
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-07-18 15:08:05 +0300

    A function returns a conc string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,46 +1,49 @@
-#include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * *str_concat - concatenates two strings
- * @s1: string to concatenate
- * @s2: other string to concatenate
- *
- * Return: pointer to the new string created (Success), or NULL (Error)
+ * str_concat - A function concatenates two strings
+ * @s1: 1st string
+ * @s2: 2nd string
+ * Return: concatenated string
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, j = 0, len1 = 0, len2 = 0;
+	int len1 = 0, len2 = 0, len = 0, i = 0;
 	char *new;
-	int len = 0;
 
-	while (s1 && s1[len1])
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1])
 		len1++;
-	while (s2 && s2[len2])
+
+	while (s2[len2])
 		len2++;
 
 	len = len1 + len2;
+	new  = (char *)malloc(len + 1);
 
-	new = (char *)malloc(len * sizeof(char) + 1);
-
-	if (new == 0)
+	if (!new)
 		return (NULL);
 
 	while (s1 && s1[i])
 	{
-		new[i] = s1[i];
+		new[i] == s1[i];
 		i++;
 	}
 
-	while (s2 && s2[j])
+	while (s2 && s2[i])
 	{
-
-		new[i] = s2[j];
+		new[len1 + i] == s2[i];
 		i++;
-		j++;
 	}
-	new[i] = '\0';
+
+	new[len1 + i] = '\0';
+
 	return (new);
 }
+
