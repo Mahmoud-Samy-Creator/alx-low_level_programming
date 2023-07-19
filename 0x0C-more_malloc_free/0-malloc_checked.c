a71b34112ccdcfb29ffe28be811873977869a22a
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-07-19 23:59:05 +0300

    A function frees 2 dim int array

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,11 +9,11 @@ void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
-	ptr = malloc(sizeof(*ptr) * b);
+	ptr = malloc(b);
 
-	if (!ptr)
+	if (ptr == NULL)
 		exit(98);
 
-	return (b);
+	return (ptr);
 }
 
