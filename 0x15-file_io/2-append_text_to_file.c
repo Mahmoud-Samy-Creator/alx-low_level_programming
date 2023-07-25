974bd020c36a793abc830c178e6f4cfa05184543
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-07-26 00:22:29 +0300

    A function append data to  afile

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,38 +1,32 @@
 #include "main.h"
 
 /**
- * append_text_to_file - appends text at the end of a file
- * @filename: filename.
- * @text_content: added content.
- *
- * Return: 1 if the file exists. -1 if the fails does not exist
- * or if it fails.
+ * append_text_to_file - A function to add content to a file
+ * @filename: the filename to add the content to
+ * @text_content: The content to be added
+ * Return: 1
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int fd, wr, con = 0;
 
 	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
-
 	if (fd == -1)
 		return (-1);
 
-	if (text_content)
-	{
-		for (nletters = 0; text_content[nletters]; nletters++)
-			;
+	if (!text_content)
+		return (1);
 
-		rwr = write(fd, text_content, nletters);
+	for (con = 0; text_content[con]; con++)
+	;
 
-		if (rwr == -1)
-			return (-1);
-	}
-
+	wr = write(fd, text_content, con);
+	if (wr == -1)
+		return (-1);
 	close(fd);
 
 	return (1);
