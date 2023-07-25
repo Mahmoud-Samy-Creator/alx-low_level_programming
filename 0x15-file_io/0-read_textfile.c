fc0b75cc68d2fa339bf475bd465fc5c8387c2750
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-07-25 23:21:08 +0300

    A function reads a text and print it

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,37 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
- *
- * Return: numbers of letters printed. It fails, returns 0.
+ * read_textfile - A function reads a text & print it
+ * @filename: The required file to be read
+ * @letters: The size will be read from the file
+ * Return: Actual number of letters
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+	ssize_t  read_num = 0, write_num = 0;
+	char *buffer;
 
 	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 		return (0);
 
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
+	buffer = malloc(letters);
+	if (!buffer)
 		return (0);
 
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
+	read_num = read(fd, buffer, letters);
+	write_num = write(STDOUT_FILENO, buffer, read_num);
 
+	if (write_num == 0)
+		return (0);
+
+	free(buffer);
 	close(fd);
 
-	free(buf);
-
-	return (nwr);
+	return (write_num);
 }
