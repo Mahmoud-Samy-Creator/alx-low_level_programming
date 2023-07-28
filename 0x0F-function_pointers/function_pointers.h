3a71dfb80c551c4f54bacf136bf5dae0fc175e1a
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-07-28 13:05:16 +0300

    functions prototypes

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef FILE_F_POINTERS
 #define FILE_F_POINTERS
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
