83d8ee27ea825ace0b35cb3d3937d43594d2ea68
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-07-29 13:30:28 +0300

    A function prints numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,19 +11,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 	va_list args;
 
-	if (n!=0)
+	va_start(args, n);
+	for (i = 0 ; i < n ; i++)
 	{
-		va_start(args, n);
-		for (i = 0 ; i < n ; i++)
-		{
-			printf("%d", va_arg(args, int));
-			if (i != n - 1)
-			{
-				if (separator)
-					printf("%s", separator);
-			}
-		}
-		printf("\n");
-		va_end(args);
+		printf("%d", va_arg(args, int));
+		if (i != n - 1 && separator != NULL)
+			printf("%s", separator);
 	}
+	printf("\n");
+	va_end(args);
 }
