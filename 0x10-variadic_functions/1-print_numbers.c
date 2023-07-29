75e0fd423d492e4ff81aeec378b31808277c965b
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-07-29 13:10:07 +0300

    A function prints numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - A function prints numbers
+ * @separator: string printed between numbers
+ * @n: number of integers passed to the function
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list args;
+
+	if (separator && n)
+	{
+		va_start(args, n);
+		for (i = 0 ; i < n ; i++)
+		{
+			printf("%d ", va_arg(args, int));
+			if (i != n - 1)
+				printf("%s", separator);
+		}
+	}
+}
