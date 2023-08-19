3e193ebebe0ca4c606db78b6f39808ebe99e77da
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-08-20 00:20:24 +0300

    A function that frees the list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * free_listint2 -  A function to free list
+ * @head: head pointer points to the first node
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *next_node;
+
+	if (*head)
+	{
+		current = *head;
+		next_node = (*head)->next;
+
+		while (next_node)
+		{
+			free(current);
+			current = next_node;
+			next_node = next_node->next;
+		}
+        free(next_node);
+        free(current);
+        current = NULL;
+        next_node = NULL;
+	}
+    else
+    {
+        printf("(nil)");
+    }
+}
