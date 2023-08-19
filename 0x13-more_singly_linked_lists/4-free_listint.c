11d1a3176333c19ca6f83db97bd9ce90dc438c87
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-08-20 00:13:29 +0300

    A function that frees the list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,22 +3,26 @@
  * free_listint -  A function to free list
  * @head: head pointer points to the first node
  * Return: void
-*/
+ */
 
 void free_listint(listint_t *head)
 {
-    listint_t *current, *next_node;
+	listint_t *current, *next_node;
 
-    if(head)
-    {
-        current = head;
-        next_node = head->next;
+	if (head)
+	{
+		current = head;
+		next_node = head->next;
 
-        while(next_node)
-        {
-            free(current);
-            current = next_node;
-            next_node = next_node->next;
-        }
-    }
+		while (next_node)
+		{
+			free(current);
+			current = next_node;
+			next_node = next_node->next;
+		}
+        free(next_node);
+        free(current);
+        current = NULL;
+        next_node = NULL;
+	}
 }
