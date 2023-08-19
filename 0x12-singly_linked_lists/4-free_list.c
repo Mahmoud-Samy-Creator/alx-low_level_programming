8343a63b4b8f7dafa680f911a987d7dabd6f3b04
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-08-19 19:54:51 +0300

    a function that frees singly linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * free_list - A function for freeing the hole list
+ * @head: head pointer points to the first node
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp = head;
+
+	if (head->next == NULL)
+	{
+		free(head);
+		head = NULL;
+	}
+	else
+	{
+		while (head->next)
+		{
+			head = head->next;
+			free(temp);
+			temp = head;
+		}
+		free(head);
+		temp = NULL;
+		head = NULL;
+	}
+}
