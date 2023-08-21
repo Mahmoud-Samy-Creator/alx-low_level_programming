aca1ea66d987cac8347a7e2c903613c367fd6e3e
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-08-21 09:49:07 +0300

    A function to delete a node at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (index != 0)
 	{
-		for (; i < index - 1 ; i++)
+		for (; i < index  ; i++)
 		{
 			prev = prev->next;
 		}
