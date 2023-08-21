10bafcc81d66101c75cbf5d3577896d36671fb68
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-08-21 09:22:14 +0300

    A function add a new node at certain position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,9 +26,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			temp = temp->next;
 			pos--;
 		}
-	}
+
 	if (temp == NULL)
 		return (NULL);
+	}
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
