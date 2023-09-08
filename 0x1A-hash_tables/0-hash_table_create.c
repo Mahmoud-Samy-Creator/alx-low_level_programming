dd2fd489c0806a55677ffc1af5e3d5fac0a63b55
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-08 20:22:27 +0300

    Task[0]

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - A function for table creation
+ * @size: the size of the table
+ * Return: A ptr to the table created
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    int i = 0
+
+    /* Allocate memory for the new hash table*/
+    hash_table_t *table = (hash_table_t*)malloc(sizeof(hash_table_t));
+    if (!table)
+        return (NULL);
+    /* Assigning the table size*/
+    table->size = size;
+
+    /* Reallocate memory for items according to the size*/
+    table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t));
+    if (!(table->array))
+        return (NULL);
+
+    for (i = 0, i < table->size ; i++)
+        table->array = NULL;
+}
