a41ca1db5fb27e7207790e7f006aacef5ea202fa
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-08 21:04:56 +0300

    Task[2]

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,9 +1,12 @@
 #include "hash_tables.h"
 /**
- * 
-*/
+ * key_index - A function to calulate the index of the key
+ * @key: the key of the node
+ * @size: the size of the array
+ * Return: key index
+ */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    return hash_djb2(key) % size;
+	return (hash_djb2(key) % size);
 }
