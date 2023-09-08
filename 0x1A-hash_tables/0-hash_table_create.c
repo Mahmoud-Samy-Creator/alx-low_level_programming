380e7c30b352a95e10d653b8298d05eeffad2b6c
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-08 20:37:08 +0300

    Task[0]

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,24 +8,26 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
     unsigned long int i = 0;
-
-    /* Allocate memory for the new hash table*/
     hash_table_t *table = (hash_table_t*)malloc(sizeof(hash_table_t));
+
     if (!table)
     {
         fprintf(stderr, "Error: malloc failed\n");
         return (NULL);
     }
-    /* Assigning the table size*/
+
     table->size = size;
 
     /* Reallocate memory for items according to the size*/
-    table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t));
+    table->array = malloc(sizeof(hash_node_t) * size);
     if (!(table->array))
-        return (NULL);
+    {
+		fprintf(stderr, "Error: malloc failed\n");
+		return (NULL);
+	}
 
-    for (i = 0; i < table->size ; i++)
-        table->array = NULL;
+    for (i = 0; i < size ; i++)
+        table->array[i] = NULL;
 
     return (table);
 }
