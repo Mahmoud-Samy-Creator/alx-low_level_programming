5c1f0db544b8e870914e1b0f68b271e30bdbd5b3
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-09 20:03:47 +0300

    Task[6]

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,20 +7,20 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i = 0;
-    hash_node_t *node, *current;
+	hash_node_t *node, *current;
 
-    for (i = 0 ; i < ht->size ; i++)
-    {
-        node = ht->array[i];
-        while(node)
-        {
-            current = node->next;
-            free(node->key);
-            free(node->value);
-            free(node);
-            node = current;
-        }
-    }
-    free(ht->array);
-    free(ht);
+	for (i = 0 ; i < ht->size ; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			current = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = current;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
