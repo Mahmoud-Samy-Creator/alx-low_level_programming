224c881d539b3488da9e3382149db5093fdc3f95
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-09 14:25:33 +0300

    Task[3]

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -35,7 +35,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	hash_node_t *node;
 
-	if (!ht || !key)
+	if (!ht || !key || strcmp(key, "") == 0)
 		return (0);
 
 	/* Create hash code for the key & determine the index */
