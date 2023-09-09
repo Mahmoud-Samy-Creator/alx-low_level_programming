5df3d72def348213d0a351220f9b98415bf0b155
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-09 19:58:52 +0300

    Task[3]

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -62,7 +62,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			}
 			else
 			{
-				strcpy(ht->array[index]->value, value);
+				free(ht->array[index]->value);
+				ht->array[index]->value = strdup(value);
 				return (1);
 			}
 		}
