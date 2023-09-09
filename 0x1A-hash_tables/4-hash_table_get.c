03cd1f7756676927cc7e4c34acc0339e0af26172
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-09 16:17:08 +0300

    Task[4]

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,19 +12,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *array;
 
-	if (!ht || !key)
-		return (NULL);
-
-	if (strcmp(key, "") == 0)
+	if (!ht)
 		return (NULL);
 
 	array = ht->array[index];
-	if (!array)
-		return (NULL);
-	
-	if (strcmp(array->key, key) == 0)
-		return (array->value);
-
 	while (array)
 	{
 		if (strcmp(array->key, key) == 0)
