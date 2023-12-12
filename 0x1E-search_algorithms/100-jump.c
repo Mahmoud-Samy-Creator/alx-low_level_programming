f867ed4b742dc4c61c86d41b13f59d0709b1580e
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-12-12 11:45:52 +0000

    In progress

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - Searches for a value in an array by Linear search algorithm
+ * @array: A pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: The value located
+ */
+int jump_search(int *array, size_t size, int value)
+{
+    int start, end, counter;
+
+    if (!array)
+        return (-1);
+
+    start = 0;
+    end = sqrt((int)size);
+
+    while (array[end] < value && start < (int)size)
+    {
+        printf("Value checked array[%d] = [%d]\n", start, array[start]);
+
+        start = end;
+        end += sqrt((int)size); 
+
+        if (end > (int)size - 1)
+            end = (int)size;
+    }
+
+    printf("Value found between indexes [%d] and [%d]\n", start, end);
+
+    for (counter = start ; counter <= end ; counter++)
+    {
+        printf("Value checked array[%d] = [%d]\n", counter, array[counter]);
+        if (array[counter] == value)
+            return (counter);
+    }
+    return (-1);
+}
