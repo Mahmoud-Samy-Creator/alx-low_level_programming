ada917a07b550f56a7efceb94f668d8dfadb2d7f
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-12-13 18:12:54 +0000

    Header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,5 +11,6 @@ int binary_search_algo(int *array, int left, int right, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif
