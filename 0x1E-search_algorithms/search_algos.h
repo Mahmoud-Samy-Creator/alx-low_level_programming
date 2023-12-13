232ac31b93183b79f8f77458b7428140d9f285d6
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-12-13 18:42:42 +0000

    Header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,6 +8,7 @@ int min(int a, int b);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int binary_search_algo(int *array, int left, int right, int value);
+int binarySearch(int *arr, int l, int r, int x);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
